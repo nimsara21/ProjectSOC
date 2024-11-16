7729357690d39da7edc9cc317ef89df07565ef61
Nimsara Nanayakkara <85825113+nimsara21@users.noreply.github.com>
2024-11-17 02:40:12 +0530

    adding Utils

--- src/main/java/com/example/usermanagementservice/util/JwtUtils.java
+++ src/main/java/com/example/usermanagementservice/util/JwtUtils.java
@@ -1,4 +1,41 @@
 package com.example.usermanagementservice.util;
 
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
 public class JwtUtils {
+    private final String jwtSecret = "NimzuprivateKey";
+    private final long jwtExpirationMs = 86400000;
+
+    public String generateJwtToken(String username){
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String getUsernameFromJwtToken(String token){
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJwt(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String token){
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJwt(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+
+
+
+
 }
